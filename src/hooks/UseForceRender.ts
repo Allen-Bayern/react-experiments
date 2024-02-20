9e62b549fabfd62bc34ad6023eb4391d22bed328
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-02-21 00:15:47 +0800

    perf: currentStatus

--- src/hooks/UseForceRender.ts
+++ src/hooks/UseForceRender.ts
@@ -2,14 +2,14 @@ import { useRef, useState, useCallback } from 'react';
 
 /** @description force render function */
 export const useForceRender = () => {
-    const [currentNum, setCurrentNum] = useState(0);
-    const currentStat = useRef(false);
+    const currentStatus = useRef(false);
+    const [currentNum, setCurrentNum] = useState(+currentStatus.current);
 
     // 强制更新函数
     const forceRender = useCallback(() => {
-        currentStat.current = !currentStat.current;
+        currentStatus.current = !currentStatus.current;
         // 在01之间反复横跳
-        setCurrentNum(+currentStat.current);
+        setCurrentNum(+currentStatus.current);
     }, []);
 
     return {
